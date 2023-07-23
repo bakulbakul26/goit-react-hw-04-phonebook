c6aef670fc3f3e8a42c08f6c241ab58e05bec5ed
Piotr Bakuliński <pbakul1807@gmail.com>
2023-07-23 17:49:38 +0200

    kkk

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
+import React, { useState } from 'react';
+import ContactForm from 'components/contactform/ContactForm';
+import ContactList from 'components/contactlist/ContactList';
+import Filter from 'components/filter/Filter';
+import useLocalStorage from './useLocalStorage';
+
 export const App = () => {
+  const [contacts, setContacts] = useLocalStorage('contacts', []);
+  const [filter, setFilter] = useState('');
+
+  const addContact = contact => {
+    const isContactExists = contacts.some(
+      c => c.name.toLowerCase() === contact.name.toLowerCase()
+    );
+
+    if (isContactExists) {
+      alert(`${contact.name} is already in contacts.`);
+      return;
+    }
+
+    setContacts(prevContacts => [...prevContacts, contact]);
+  };
+
+  const deleteContact = id => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== id)
+    );
+  };
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
         justifyContent: 'center',
+        flexDirection: 'column',
         alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
+      <ContactForm addContact={addContact} />
+
+      <h2>Contacts</h2>
+      <Filter filter={filter} onChangeFilter={setFilter} />
+      {filteredContacts.length > 0 ? (
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={deleteContact}
+        />
+      ) : (
+        <p>No contacts found.</p>
+      )}
     </div>
   );
 };
+
+export default App;
